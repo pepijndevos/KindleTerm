7a7415fb20286fec13d434ffbbe7a5420d64e598
Pepijn de Vos <pepijndevos@gmail.com>
2012-10-08 14:14:19 +0200

    properties file

--- src/kindle/TermKindlet.java
+++ src/kindle/TermKindlet.java
@@ -30,10 +30,12 @@ import java.awt.Container;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
+import java.io.FileInputStream;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
+import java.util.Properties;
 
 /**
  * Implementation of the lifecycle of the KindleTerm.
@@ -53,6 +55,26 @@ public class TermKindlet extends AbstractKindlet
     private RemoteKbdReceiver remoteKeyboard;
 
     private String host = "KindleTermHD";
+
+    private Properties props;
+
+    private void readProps() {
+	Properties defaults = new Properties();
+	defaults.setProperty("host", "127.0.0.1");
+	defaults.setProperty("port", "23");
+	defaults.setProperty("cmd", "");
+
+	props = new Properties(defaults);
+
+	try {
+		File path = new File(ctx.getHomeDirectory(), "kindleterm.properties");
+		FileInputStream file = new FileInputStream(path);
+		props.load(file);
+	} catch(java.io.IOException e) {
+		// well, defaults then
+	}
+
+    }
 
     public void create(KindletContext context) {
         try {
@@ -83,7 +105,23 @@ public class TermKindlet extends AbstractKindlet
             root.validate();
             root.setVisible(true);
 
-            session.connect("127.0.0.1", 23);
+	    readProps();
+
+            session.connect(props.getProperty("host"),
+			    Integer.parseInt(props.getProperty("port")));
+
+	    if(props.getProperty("login") != null && props.getProperty("password") != null) {
+		    Thread.sleep(1000);
+		    session.typeString(props.getProperty("login") + "\n");
+		    Thread.sleep(1000);
+		    session.typeString(props.getProperty("password") + "\n");
+		    Thread.sleep(1000);
+	    }
+	    
+	    if(props.getProperty("cmd") != null) {
+		    String cmd = props.getProperty("cmd");
+		    session.typeString(cmd + "\n");
+	    }
 
             remoteKeyboard = new RemoteKbdReceiver(3333, term, term);
             remoteKeyboard.setRKbdStatusListener(TermKindlet.this);
